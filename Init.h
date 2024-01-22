bb0767ebc39a969bc36b83e7af2f590d0c54a4b0
AdamJamesinley <adam.finley.94@gmail.com>
2024-01-22 12:01:47 +0100

    back to T=1

--- Init.h
+++ Init.h
@@ -83,9 +83,9 @@ void initSodX(Array Q, int i, int j, const DeviceParams &params)
   void initTriLayer(Array Q, int i, int j, const Params &params, const RandomPool &random_pool) {
     Pos pos = getPos(params, i, j);
     const real_t y = pos[IY];
-    
-    const real_t T0 = 10.0;
-    const real_t rho0 = 10.0;
+
+    const real_t T0 = 1.0;
+    const real_t rho0 = 1.0;
     const real_t p0 = rho0 * T0;
 
     const real_t T1   = T0 + params.theta2 * params.tri_y1;
@@ -95,7 +95,7 @@ void initSodX(Array Q, int i, int j, const DeviceParams &params)
     const real_t T2   = T1 + params.theta1 * (params.tri_y2-params.tri_y1);
     const real_t rho2 = rho1 * pow(T2/T1, params.m1);
     const real_t p2   = p1 * pow(T2/T1, params.m1+1.0);
-    
+
     // Top layer
     real_t T;
     if (y <= params.tri_y1) {
@@ -104,7 +104,7 @@ void initSodX(Array Q, int i, int j, const DeviceParams &params)
       Q(j, i, IU) = 0.0;
       Q(j, i, IV) = 0.0;
       Q(j, i, IP) = p0 * pow(T/T0, params.m2+1.0);
-    } 
+    }
     // Middle layer
     else if (y <= params.tri_y2) {
       auto generator = random_pool.get_state();
@@ -127,7 +127,7 @@ void initSodX(Array Q, int i, int j, const DeviceParams &params)
       Q(j, i, IV) = 0.0;
       Q(j, i, IP) = p2 * pow(T/T2, params.m2+1.0);
     }
-  } 
+  }
 }
 
 /**

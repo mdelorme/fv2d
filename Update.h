69eef11e30280da91acd8985f56075c2e31317a2
Lucas Barbier <lucasbarbr@outlook.com>
2025-02-20 16:32:07 +0100

    condition riemann solver

--- Update.h
+++ Update.h
@@ -48,6 +48,9 @@ public:
       slopesY = Array("SlopesY", params.Nty, params.Ntx, Nfields);
 
       /** TODO Lucas : Check qu'on n'essaie pas d'utiliser hllc/hll avec un run MHD */
+      if (mhd_run && (params.riemann_solver==HLL || params.riemann_solver==HLLD)){
+        throw std::runtime_error("HLL and HLLD are not supported for MHD runs.")
+      }
     };
   ~UpdateFunctor() = default;
 

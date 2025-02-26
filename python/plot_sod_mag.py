ea9df2c548dbf74808a82f24b505bd46e69d5de5
lukbrb <lucasbarbr@outlook.com>
2025-02-26 18:35:36 +0100

    allow 1D and 2D plots

--- python/plot_sod_mag.py
+++ python/plot_sod_mag.py
@@ -1,28 +1,48 @@
 import h5py
 import matplotlib.pyplot as plt
 
-
+Nx = 64
+Ny = 1
 with h5py.File('build/run.h5', 'r') as fichier:
     print("Clés disponibles :", list(fichier.keys()))
     Nite = len(fichier.keys())-2 # removing x and y
+    if Ny < 2:
+        x = fichier['x'][:Nx]
+        for num in range(Nite):
+            p = fichier[f'ite_{num:04d}']['prs'][:][0]
+            r = fichier[f'ite_{num:04d}']['rho'][:][0]
+            u = fichier[f'ite_{num:04d}']['u'][:][0]
+            v = fichier[f'ite_{num:04d}']['v'][:][0]
+            w = fichier[f'ite_{num:04d}']['w'][:][0]
+            by = fichier[f'ite_{num:04d}']['by'][:][0]
+            bz = fichier[f'ite_{num:04d}']['bz'][:][0]
 
-    x = fichier['x'][:64]
-    for num in range(Nite):
-        p = fichier[f'ite_{num:04d}']['prs'][:].reshape(64)
-        r = fichier[f'ite_{num:04d}']['rho'][:].reshape(64)
-        u = fichier[f'ite_{num:04d}']['u'][:].reshape(64)
-        v = fichier[f'ite_{num:04d}']['v'][:].reshape(64)
-        w = fichier[f'ite_{num:04d}']['w'][:].reshape(64)
-        by = fichier[f'ite_{num:04d}']['by'][:].reshape(64)
-        bz = fichier[f'ite_{num:04d}']['bz'][:].reshape(64)
+            fig, ax = plt.subplots(3, 2, figsize=(15, 10))
+            ax[0,0].plot(x, r)
+            ax[0,1].plot(x, p)
+            ax[1,0].plot(x, u)
+            ax[1,1].plot(x, v)
+            ax[2,0].plot(x, by)
+            ax[2,1].plot(x, bz)
+            ax[0,0].set_title('Density')
+            ax[0,1].set_title('Pressure')
+            ax[1,0].set_title('$v_x$')
+            ax[1,1].set_title('$v_y$')
+            ax[2,0].set_title('$B_y$')
+            ax[2,1].set_title('$B_z$')
+            plt.tight_layout()
+            plt.savefig(f"ite__{num}.png")
+            plt.close()
+    else:
+        fig, ax = plt.subplots(3, 2, figsize=(10, 15))
+        Nite -= 1
+        ax[0,0].imshow(fichier[f'ite_{Nite:04d}']['rho'])
+        ax[0,1].imshow(fichier[f'ite_{Nite:04d}']['prs'])
+        ax[1,0].imshow(fichier[f'ite_{Nite:04d}']['u'])
+        ax[1,1].imshow(fichier[f'ite_{Nite:04d}']['v'])
+        ax[2,0].imshow(fichier[f'ite_{Nite:04d}']['by'])
+        ax[2,1].imshow(fichier[f'ite_{Nite:04d}']['bz'])
 
-        fig, ax = plt.subplots(3, 2, figsize=(15, 10))
-        ax[0,0].plot(x, r)
-        ax[0,1].plot(x, p)
-        ax[1,0].plot(x, u)
-        ax[1,1].plot(x, v)
-        ax[2,0].plot(x, by)
-        ax[2,1].plot(x, bz)
         ax[0,0].set_title('Density')
         ax[0,1].set_title('Pressure')
         ax[1,0].set_title('$v_x$')
@@ -30,5 +50,5 @@ with h5py.File('build/run.h5', 'r') as fichier:
         ax[2,0].set_title('$B_y$')
         ax[2,1].set_title('$B_z$')
         plt.tight_layout()
-        plt.savefig(f"ite__{num}.png")
+        plt.show()
         plt.close()

c7a056727d7469cce2b60660c82c2db0d3023f84
Lucas Barbier <lucasbarbr@outlook.com>
2025-03-12 17:12:56 +0100

    writes 3D vecs if MHD

--- IOManager.h
+++ IOManager.h
@@ -60,14 +60,25 @@ namespace fv2d {
           <DataItem Dimensions="%d %d" NumberType="Float" Precision="8" Format="HDF">%s:/%s/%s</DataItem>
         </DataItem>
       </Attribute>)xml";
-    #define format_xdmf_vector_field(params, path, group, name, field_x, field_y) \
-            name,                                                           \
-            params.Ny, params.Nx,                                           \
-            params.Ny, params.Nx,                                           \
-            (path + ".h5").c_str(), group.c_str(), field_x,  \
-            params.Ny, params.Nx,                                           \
-            (path + ".h5").c_str(), group.c_str(), field_y
-
+    #ifndef MHD
+      #define format_xdmf_vector_field(params, path, group, name, field_x, field_y) \
+              name,                                                           \
+              params.Ny, params.Nx,                                           \
+              params.Ny, params.Nx,                                           \
+              (path + ".h5").c_str(), group.c_str(), field_x,  \
+              params.Ny, params.Nx,                                           \
+              (path + ".h5").c_str(), group.c_str(), field_y
+    #else
+      #define format_xdmf_vector_field(params, path, group, name, field_x, field_y, field_z) \
+              name,                                                           \
+              params.Ny, params.Nx,                                           \
+              params.Ny, params.Nx,                                           \
+              (path + ".h5").c_str(), group.c_str(), field_x,                 \
+              params.Ny, params.Nx,                                           \
+              (path + ".h5").c_str(), group.c_str(), field_y,                  \
+              params.Ny, params.Nx,                                           \
+              (path + ".h5").c_str(), group.c_str(), field_z
+    #endif // MHD
     char str_xdmf_ite_footer[] =
     R"xml(
     </Grid>
@@ -77,9 +88,10 @@ namespace fv2d {
 class IOManager {
 public:
   Params params;
+  BoundaryManager bc_manager;
 
   IOManager(Params &params)
-    : params(params) {};
+    : params(params), bc_manager(params) {};
 
   ~IOManager() = default;
 
@@ -125,12 +137,13 @@ public:
     file.createDataSet("y", y);
 
     using Table = std::vector<real_t>;
-
+    bc_manager.fillBoundaries(Q);
     auto Qhost = Kokkos::create_mirror(Q);
     Kokkos::deep_copy(Qhost, Q);
 
     Table trho, tu, tv, tprs;
     Table tw, tbx, tby, tbz; // TODO: Ajouter un condition sur régime MHD
+    std::vector<Table> divB(params.Ntx, Table(params.Nty, 0.0));
     for (int j=params.jbeg; j<params.jend; ++j) {
 
       for (int i=params.ibeg; i<params.iend; ++i) {
@@ -143,28 +156,35 @@ public:
         tu.push_back(u);
         tv.push_back(v);
         tprs.push_back(p);
-// TODO: Ajouter un condition sur régime MHD
-        real_t w  = Qhost(j, i, IW);
-        real_t bx = Qhost(j, i, IBX);
-        real_t by = Qhost(j, i, IBY);
-        real_t bz = Qhost(j, i, IBZ);
 
-        tw.push_back(w);
-        tbx.push_back(bx);
-        tby.push_back(by);
-        tbz.push_back(bz);
+        if (mhd_run){
+          real_t w  = Qhost(j, i, IW);
+          real_t bx = Qhost(j, i, IBX);
+          real_t by = Qhost(j, i, IBY);
+          real_t bz = Qhost(j, i, IBZ);
+          
+          tw.push_back(w);
+          tbx.push_back(bx);
+          tby.push_back(by);
+          tbz.push_back(bz);
+          real_t dBx_dx = (Qhost(j+1, i, IBX) - Qhost(j-1, i, IBX)) / (2 * params.dx);
+          real_t dBy_dy = (Qhost(j, i+1, IBY) - Qhost(j, i-1, IBY)) / (2 * params.dy);
+          divB[i][j] = dBx_dx + dBy_dy;
+        }
+        }
       }
-    }
 
     file.createDataSet("rho", trho);
     file.createDataSet("u", tu);
     file.createDataSet("v", tv);
     file.createDataSet("prs", tprs);
-    //TODO: Ajouter un condition sur régime MHD
-    file.createDataSet("w", tw);
-    file.createDataSet("bx", tbx);
-    file.createDataSet("by", tby);
-    file.createDataSet("bz", tbz);
+    if (mhd_run) {
+      file.createDataSet("w", tw);
+      file.createDataSet("bx", tbx);
+      file.createDataSet("by", tby);
+      file.createDataSet("bz", tbz);
+      file.createDataSet("divB", divB);
+    }
     file.createAttribute("time", t);
 
     std::string empty_string = "";
@@ -172,11 +192,13 @@ public:
     fprintf(xdmf_fd, str_xdmf_header, format_xdmf_header(params, path));
     fprintf(xdmf_fd, str_xdmf_ite_header, t);
     fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, path, empty_string, "rho"));
-    // TODO: Ajouter un condition sur régime MHD
-    fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, path, empty_string, "velocity", "u", "v"));
-    fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, path, empty_string, "w"));
-    fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, path, empty_string, "bx"));
-    fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, path, empty_string, "magnetic_field", "by", "bz"));
+    #ifdef MHD
+      fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, path, empty_string, "velocity", "u", "v", "w"));
+      fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, path, empty_string, "magnetic_field", "bx", "by", "bz"));    
+      fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, path, empty_string, "divB"));
+    #else
+      fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, path, empty_string, "velocity", "u", "v"));
+    #endif
     fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, path, empty_string, "prs"));
     fprintf(xdmf_fd, "%s", str_xdmf_ite_footer);
     fprintf(xdmf_fd, "%s", str_xdmf_footer);
@@ -221,12 +243,13 @@ public:
     }
 
     using Table = std::vector<std::vector<real_t>>;
-
+    bc_manager.fillBoundaries(Q);
     auto Qhost = Kokkos::create_mirror(Q);
     Kokkos::deep_copy(Qhost, Q);
 
     Table trho, tu, tv, tprs;
     Table tw, tbx, tby, tbz; // TODO: Ajouter un condition sur régime MHD
+    Table divB(params.Ntx, std::vector<real_t>(params.Nty, 0.0));
     for (int j=params.jbeg; j<params.jend; ++j) {
       std::vector<real_t> rrho, ru, rv, rprs;
       std::vector<real_t> rw, rbx, rby, rbz; // TODO: Ajouter un condition sur régime MHD
@@ -240,8 +263,9 @@ public:
         ru.push_back(u);
         rv.push_back(v);
         rprs.push_back(p);
-        
-        real_t w  = Qhost(j, i, IW); // TODO: Ajouter un condition sur régime MHD
+
+        if (mhd_run){
+        real_t w  = Qhost(j, i, IW);
         real_t bx = Qhost(j, i, IBX);
         real_t by = Qhost(j, i, IBY);
         real_t bz = Qhost(j, i, IBZ);
@@ -250,37 +274,48 @@ public:
         rbx.push_back(bx);
         rby.push_back(by);
         rbz.push_back(bz);
+        real_t dBx_dx = (Qhost(j+1, i, IBX) - Qhost(j-1, i, IBX)) / (2 * params.dx);
+        real_t dBy_dy = (Qhost(j, i+1, IBY) - Qhost(j, i-1, IBY)) / (2 * params.dy);
+        divB[i][j] = dBx_dx + dBy_dy;
+        }
       }
-
       trho.push_back(rrho);
       tu.push_back(ru);
       tv.push_back(rv);
       tprs.push_back(rprs);
-      tw.push_back(rw); // TODO: Ajouter un condition sur régime MHD
+
+      if (mhd_run){
+      tw.push_back(rw);
       tbx.push_back(rbx);
       tby.push_back(rby);
       tbz.push_back(rbz);
-    }
+      }
+  } // loop j
 
     auto group = file.createGroup(path);
     group.createDataSet("rho", trho);
     group.createDataSet("u", tu);
     group.createDataSet("v", tv);
     group.createDataSet("prs", tprs);
-    group.createDataSet("w", tw); // TODO: Ajouter un condition sur régime MHD
-    group.createDataSet("bx", tbx);
-    group.createDataSet("by", tby);
-    group.createDataSet("bz", tbz);
+    if (mhd_run){
+      group.createDataSet("w", tw); // TODO: Ajouter un condition sur régime MHD
+      group.createDataSet("bx", tbx);
+      group.createDataSet("by", tby);
+      group.createDataSet("bz", tbz);
+      group.createDataSet("divB", divB);
+    }
     group.createAttribute("time", t);
 
     fseek(xdmf_fd, -sizeof(str_xdmf_footer), SEEK_END);
     fprintf(xdmf_fd, str_xdmf_ite_header, t);
     fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, params.filename_out, path, "rho"));
-    // TODO: Ajouter un condition sur régime MHD
+    #ifdef MHD
+      fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, params.filename_out, path, "velocity", "u", "v", "w"));
+      fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, params.filename_out, path, "magnetic_field", "bx", "by", "bz"));
+      fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, params.filename_out, path, "divB"));
+    #else
     fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, params.filename_out, path, "velocity", "u", "v"));
-    fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(params, params.filename_out, path, "magnetic_field", "by", "bz"));
-    fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, params.filename_out, path, "w"));
-    fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, params.filename_out, path, "bx"));
+    #endif
     fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(params, params.filename_out, path, "prs"));
     fprintf(xdmf_fd, "%s", str_xdmf_ite_footer);
     fprintf(xdmf_fd, "%s", str_xdmf_footer);
@@ -317,10 +352,12 @@ public:
     load_and_copy("u", IU);
     load_and_copy("v", IV);
     load_and_copy("prs", IP);
-    // load_and_copy("w", IW); // TODO: Ajouter un condition sur régime MHD
-    // load_and_copy("bx", IBX);
-    // load_and_copy("by", IBY);
-    // load_and_copy("bz", IBZ);
+    if (mhd_run){
+      load_and_copy("w", IW);
+      load_and_copy("bx", IBX);
+      load_and_copy("by", IBY);
+      load_and_copy("bz", IBZ);
+    }
     Kokkos::deep_copy(Q, Qhost);
 
     std::cout << "Restart finished !" << std::endl;

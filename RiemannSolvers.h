3f14621caca418d5c5d86338dea9cd51300a4cff
Maxime Delorme <maxime.delorme@gmail.com>
2025-05-16 08:48:52 +0200

    Fixed merge problem

--- RiemannSolvers.h
+++ RiemannSolvers.h
@@ -131,7 +131,7 @@ void hllc(State &qL, State &qR, State &flux, real_t &pout, const DeviceParams &p
  * dynamics", Journal of Computational Physics, vol 496.
  */
 KOKKOS_INLINE_FUNCTION
-void fslp(State &qL, State &qR, State &flux, real_t &pout, real_t gdx, const DeviceParams &params) {
+void fslp(State &qL, State &qR, State &flux, real_t &pout, real_t gdx, int side, const DeviceParams &params) {
   // 1. Basic quantities
   const real_t rhoL = qL[IR];
   const real_t uL   = qL[IU];
@@ -149,10 +149,10 @@ void fslp(State &qL, State &qR, State &flux, real_t &pout, real_t gdx, const Dev
   const real_t ai = params.fslp_K * Kokkos::max(rhoL*csL, rhoR*csR);           // eq (17)
   const real_t theta = Kokkos::min(1.0, Kokkos::max(Kokkos::abs(uL)/csL, Kokkos::abs(uR)/csR));  // eq (77)
 
-  // 3. Calculating u* and PI*
-  //                                                  vvv this minus comes from g = -grad phi
-  const real_t ustar = 0.5*(uR+uL) - 0.5 / ai * (pR-pL - 0.5*(rhoL+rhoR)*gdx);  // eq (15)
-  const real_t Pi    = 0.5*(pR+pL) - theta * 0.5*ai*(uR-uL);                    // eq (15)
+  // 3. Calculating M, u* and PI*
+  const real_t M = 0.5 * (rhoL + rhoR) * gdx; // Momentum term for gravity
+  const real_t ustar = 0.5*(uR+uL) - 0.5 / ai * (pR-pL - M);  // eq (15)
+  const real_t Pi    = 0.5*(pR+pL) - theta * 0.5*ai*(uR-uL);  // eq (15)
 
   // 4. Upwinding
   const State& qstar = (ustar > 0 ? qL : qR);                                         // eq (32)
@@ -161,11 +161,10 @@ void fslp(State &qL, State &qR, State &flux, real_t &pout, real_t gdx, const Dev
 
   // 5. Calculating flux : eq (31)
   flux[IR] = ustar*qstar[IR];
-  flux[IU] = ustar*qstar[IR]*qstar[IU] + Pi;
-  flux[IV] = ustar*qstar[IR]*qstar[IV];
-  flux[IE] = ustar*(Estar + Pi);
-
-  //printf("ai=%lf; theta=%lf; csL=%lf; csR=%lf; ustar=%lf; Pi=%lf\n", ai, theta, csL, csR, ustar, Pi);
+  flux[IU] = ustar*qstar[IR]*qstar[IU] + Pi - side*M*0.5;
+  flux[IV] = ustar*qstar[IR]*qstar[IV] ;
+  flux[IE] = ustar*(Estar + Pi) - side*M*ustar*0.5;
+  pout = Pi;
 }
 
 }

0f04bd902c2b193702c447e19d15d60f34d7752a
Lucas Barbier <lucasbarbr@outlook.com>
2025-05-23 17:47:33 +0200

    simplify code with vectors and make special FluxKepec function

--- StatesMHD.h
+++ StatesMHD.h
@@ -31,7 +31,8 @@ State primToCons(State &q, const DeviceParams &params) {
 
   real_t Ek = 0.5 * q[IR] * (q[IU]*q[IU] + q[IV]*q[IV] + q[IW]*q[IW]);
   real_t Em = 0.5 * (q[IBX]*q[IBX] + q[IBY]*q[IBY] + q[IBZ]*q[IBZ]);
-  res[IE] = Ek + Em + q[IP] / (params.gamma0-1.0);
+  real_t Epsi = (params.riemann_solver==IDEALGLM ? 0.5*q[IPSI]*q[IPSI] : 0.0);
+  res[IE] = Ek + Em + Epsi + q[IP] / (params.gamma0-1.0);
   res[IBX] = q[IBX];
   res[IBY] = q[IBY];
   res[IBZ] = q[IBZ];
@@ -51,7 +52,9 @@ State consToPrim(State &u, const DeviceParams &params) {
 
   real_t Ek = 0.5 * res[IR] * (res[IU]*res[IU] + res[IV]*res[IV] + res[IW]*res[IW]);
   real_t Em = 0.5 * (u[IBX]*u[IBX] + u[IBY]*u[IBY] + u[IBZ]*u[IBZ]);
-  res[IP] = (u[IE] - Ek - Em) * (params.gamma0-1.0);
+  real_t Epsi = (params.riemann_solver==IDEALGLM ? 0.5*u[IPSI]*u[IPSI] : 0.0);
+ 
+  res[IP] = (u[IE] - Ek - Em - Epsi) * (params.gamma0-1.0);
   res[IBX] = u[IBX];
   res[IBY] = u[IBY];
   res[IBZ] = u[IBZ];
@@ -73,6 +76,21 @@ real_t fastMagnetoAcousticSpeed(State &q, const DeviceParams &params, IDir idir)
   const real_t cap2 = q[IBX+idir]*q[IBX+idir]/q[IR];
   return sqrt(0.5*(c02+ca2)+0.5*sqrt((c02+ca2)*(c02+ca2)-4.0*c02*cap2));
 }
+
+KOKKOS_INLINE_FUNCTION
+real_t logMean(const real_t xl, const real_t xr, const real_t epsilon = 1e-2){
+    const real_t zeta = xl/xr;
+    const real_t f = (zeta - 1.0) / (zeta + 1.0);
+    const real_t u = f * f;
+    real_t F;
+    if (u < epsilon){
+      F = 1.0 + u/3.0 + u*u/5.0 + u*u*u/7.0;
+    }
+    else{
+      F = Kokkos::log(zeta) / 2.0 / f;
+    }
+    return (xr + xl) / (2 * F);
+  };
 
 KOKKOS_INLINE_FUNCTION
 State& operator+=(State &a, State b) {

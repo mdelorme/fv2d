c7121c14477629f35a95c50092f4542ecab729a7
Lucas Barbier <lucasbarbr@outlook.com>
2025-05-27 15:51:55 +0200

    correction et simplification des sources

--- Sources.h
+++ Sources.h
@@ -17,7 +17,9 @@ public:
         auto params = full_params.device_params;
         const real_t dx = params.dx;
         const real_t dy = params.dy;
-
+        real_t ch_global = 0.0;
+        if (mhd_run && params.riemann_solver == IDEALGLM)
+            ch_global = ComputeGlobalDivergenceSpeed(Q, full_params);
         Kokkos::parallel_for(
             "Apply sources",
             full_params.range_dom,
@@ -31,29 +33,29 @@ public:
                 }
                 if (params.riemann_solver == IDEALGLM) {
                     // Ideal GLM source term
-                    real_t l;
                     State q = getStateFromArray(Q, i, j);
-                    real_t ch = ComputeGlobalDivergenceSpeed(Q, full_params);
-                    real_t alpha = ch/params.cr;
-
+                    real_t alpha = ch_global/params.cr;
+                    
                     // TODO: passer à un ordre 2 en espace pour la PLM
                     State SMag{}, SPhi{};
+                    
                     // Magnetic divergence source term
                     State VMag {0.0, q[IBX], q[IBY], q[IBZ], q[IU]*q[IBX] + q[IV]*q[IBY] + q[IW]*q[IBZ], q[IU], q[IV], q[IW], 0.0};
                     SMag = (0.5 * (Q(j, i+1, IBX) - Q(j, i-1, IBX)) / dx) * VMag + (0.5 * (Q(j+1, i, IBY) - Q(j-1, i, IBY)) / dy) * VMag;
+                    
                     // Psi correlated non-conservative term
                     State SPhiX {0.0, 0.0, 0.0, 0.0, q[IU]*q[IPSI], 0.0, 0.0, 0.0, q[IU]};
                     SPhiX *= 0.5 * (Q(j, i+1, IPSI) - Q(j, i-1, IPSI)) / dx;
                     State SPhiY {0.0, 0.0, 0.0, 0.0, q[IV]*q[IPSI], 0.0, 0.0, 0.0, q[IV]};
                     SPhiY *= 0.5 * (Q(j+1, i, IPSI) - Q(j-1, i, IPSI)) / dy;
                     SPhi = SPhiX + SPhiY;
+                    
                     // Parabolic source term
                     State SParabolic {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, alpha*q[IPSI]};
-
+                    
                     for (int ivar = 0; ivar < Nfields; ++ivar) {
                         Unew(j, i, ivar) += dt * (SMag[ivar] + SPhi[ivar] + SParabolic[ivar]);
                     }
-
                 }
             });
     }

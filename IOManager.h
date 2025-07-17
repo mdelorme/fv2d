2497848abe2720c65039c8f00e688ddc13fed64f
ggdoe <gregoire.doebele@ens-paris-saclay.fr>
2025-07-17 23:41:26 +0200

    merge saveSolutionUnique and saveSolutionMultiple into saveSolution_aux<>

--- IOManager.h
+++ IOManager.h
@@ -95,105 +95,45 @@ public:
 
   ~IOManager() = default;
 
+  enum SaveMethod { IO_UNIQUE, IO_MULTIPLE };
+  
   void saveSolution(const Array &Q, int iteration, real_t t) {
     if (params.multiple_outputs)
-      saveSolutionMultiple(Q, iteration, t);
+      saveSolution_aux<IO_MULTIPLE>(Q, iteration, t);
     else
-      saveSolutionUnique(Q, iteration, t);
+      saveSolution_aux<IO_UNIQUE>(Q, iteration, t);
   }
 
-  void saveSolutionMultiple(const Array &Q, int iteration, real_t t)
+  template<enum SaveMethod save_method>
+  void saveSolution_aux(const Array &Q, int iteration, real_t t)
   {
+    constexpr bool is_multiple = (save_method == IO_MULTIPLE);
     std::ostringstream oss;
     
-    oss << params.filename_out << "_" << std::setw(ite_nzeros) << std::setfill('0') << iteration;
-    std::string iteration_str = oss.str();
-    std::string h5_filename  = oss.str() + ".h5";
-    std::string xmf_filename = oss.str() + ".xmf";
-    std::string output_path = params.output_path + "/";
-
-    File file(output_path + h5_filename, File::Truncate);
-    FILE* xdmf_fd = fopen((output_path + xmf_filename).c_str(), "w+");
-
-    file.createAttribute("Ntx",  device_params.Ntx);
-    file.createAttribute("Nty",  device_params.Nty);
-    file.createAttribute("Nx",   device_params.Nx);
-    file.createAttribute("Ny",   device_params.Ny);
-    file.createAttribute("ibeg", device_params.ibeg);
-    file.createAttribute("iend", device_params.iend);
-    file.createAttribute("jbeg", device_params.jbeg);
-    file.createAttribute("jend", device_params.jend);
-    file.createAttribute("problem", params.problem);
-
-    std::vector<real_t> x, y;
-    // -- vertex pos
-    for (int j=device_params.jbeg; j <= device_params.jend; ++j) {
-      for (int i=device_params.ibeg; i <= device_params.iend; ++i) {
-        x.push_back((i-device_params.ibeg) * device_params.dx + device_params.xmin);
-        y.push_back((j-device_params.jbeg) * device_params.dy + device_params.ymin);
-      }
+    std::string iteration_str, basename;
+    if constexpr (is_multiple) {
+      oss << params.filename_out << "_" << std::setw(ite_nzeros) << std::setfill('0') << iteration;
+      iteration_str = oss.str();
+      basename = oss.str();
     }
-
-    file.createDataSet("x", x);
-    file.createDataSet("y", y);
-
-    using Table = std::vector<real_t>;
-
-    auto Qhost = Kokkos::create_mirror(Q);
-    Kokkos::deep_copy(Qhost, Q);
-
-    Table trho, tu, tv, tprs;
-    for (int j=device_params.jbeg; j<device_params.jend; ++j) {
-
-      for (int i=device_params.ibeg; i<device_params.iend; ++i) {
-        real_t rho = Qhost(j, i, IR);
-        real_t u   = Qhost(j, i, IU);
-        real_t v   = Qhost(j, i, IV);
-        real_t p   = Qhost(j, i, IP);
-
-        trho.push_back(rho);
-        tu.push_back(u);
-        tv.push_back(v);
-        tprs.push_back(p);
-      }
+    else {
+      oss << ite_prefix << std::setw(ite_nzeros) << std::setfill('0') << iteration;
+      iteration_str = oss.str();
+      basename = params.filename_out;
     }
-
-    file.createDataSet("rho", trho);
-    file.createDataSet("u", tu);
-    file.createDataSet("v", tv);
-    file.createDataSet("prs", tprs);
-    file.createAttribute("time", t);
-    file.createAttribute("iteration", iteration);
-
-    std::string group = "";
-
-    fprintf(xdmf_fd, str_xdmf_header, format_xdmf_header(device_params, h5_filename));
-    fprintf(xdmf_fd, str_xdmf_ite_header, format_xdmf_ite_header(iteration_str, t));
-    fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(group, "rho"));
-    fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(group, "velocity", "u", "v"));
-    fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(group, "prs"));
-    fprintf(xdmf_fd, "%s", str_xdmf_ite_footer);
-    fprintf(xdmf_fd, "%s", str_xdmf_footer);
-    fclose(xdmf_fd);
-  }
-
-  void saveSolutionUnique(const Array &Q, int iteration, real_t t) {
-    std::ostringstream oss;
     
-    oss << ite_prefix << std::setw(ite_nzeros) << std::setfill('0') << iteration;
-    std::string iteration_str = oss.str();
-    std::string h5_filename  = params.filename_out + ".h5";
-    std::string xmf_filename = params.filename_out + ".xmf";
-    std::string output_path = params.output_path + "/";
+    const std::string h5_filename  = basename + ".h5";
+    const std::string xmf_filename = basename + ".xmf";
+    const std::string output_path = params.output_path + "/";
 
-    force_file_truncation = (force_file_truncation || iteration == 0);
+    const bool write_attrs = (is_multiple || force_file_truncation || iteration == 0);
       
-    auto flag_h5 = (force_file_truncation ? File::Truncate : File::ReadWrite);
-    auto flag_xdmf = (force_file_truncation ? "w+" : "r+");
+    auto flag_h5 = (write_attrs ? File::Truncate : File::ReadWrite);
+    auto flag_xdmf = (write_attrs ? "w+" : "r+");
     File file(output_path + h5_filename, flag_h5);
     FILE* xdmf_fd = fopen((output_path + xmf_filename).c_str(), flag_xdmf);
 
-    if (force_file_truncation) {
+    if (write_attrs) {
       force_file_truncation = false;
       file.createAttribute("Ntx", device_params.Ntx);
       file.createAttribute("Nty", device_params.Nty);
@@ -207,17 +147,18 @@ public:
 
       std::vector<real_t> x, y;
       // -- vertex pos
-      for (int j=device_params.jbeg; j <= device_params.jend; ++j)
-        for (int i=device_params.ibeg; i <= device_params.iend; ++i)
-        {
-          x.push_back((i-device_params.ibeg) * device_params.dx + device_params.xmin);
-          y.push_back((j-device_params.jbeg) * device_params.dy + device_params.ymin);
+      for (int j=device_params.jbeg; j <= device_params.jend; ++j) {
+        for (int i=device_params.ibeg; i <= device_params.iend; ++i) {
+          x.push_back((i-device_params.ibeg) * device_params.dx);
+          y.push_back((j-device_params.jbeg) * device_params.dy);
         }
+      }
       file.createDataSet("x", x);
       file.createDataSet("y", y);
 
-      fprintf(xdmf_fd, str_xdmf_header, format_xdmf_header(device_params, params.filename_out + ".h5"));
-      fprintf(xdmf_fd, "%s", str_xdmf_footer);
+      fprintf(xdmf_fd, str_xdmf_header, format_xdmf_header(device_params, h5_filename));
+      if constexpr (!is_multiple) 
+        fprintf(xdmf_fd, "%s", str_xdmf_footer);
     }
     
     using Table = std::vector<real_t>;
@@ -240,17 +181,32 @@ public:
       }
     }
 
-    auto ite_group = file.createGroup(iteration_str);
-    ite_group.createDataSet("rho", trho);
-    ite_group.createDataSet("u", tu);
-    ite_group.createDataSet("v", tv);
-    ite_group.createDataSet("prs", tprs);
-    ite_group.createAttribute("time", t);
-    ite_group.createAttribute("iteration", iteration);
+    auto save_groups = [&](auto &g) {
+      g.createDataSet("rho", trho);
+      g.createDataSet("u", tu);
+      g.createDataSet("v", tv);
+      g.createDataSet("prs", tprs);
+      g.createAttribute("time", t);
+      g.createAttribute("iteration", iteration);
+    };
 
-    const std::string group = iteration_str + "/";
+    if constexpr (is_multiple) {
+      save_groups(file);
+    }
+    else {
+      auto h5_group = file.createGroup(iteration_str);
+      save_groups(h5_group);
+    }
 
-    fseek(xdmf_fd, -sizeof(str_xdmf_footer), SEEK_END);
+    std::string group;
+    if constexpr (is_multiple) {
+      group = "";
+    }
+    else {
+      group = iteration_str + "/";
+      fseek(xdmf_fd, -sizeof(str_xdmf_footer), SEEK_END);
+    }
+
     fprintf(xdmf_fd, str_xdmf_ite_header, format_xdmf_ite_header(iteration_str, t));
     fprintf(xdmf_fd, str_xdmf_scalar_field, format_xdmf_scalar_field(group, "rho"));
     fprintf(xdmf_fd, str_xdmf_vector_field, format_xdmf_vector_field(group, "velocity", "u", "v"));

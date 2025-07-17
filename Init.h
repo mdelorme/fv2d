8d448a004ac9e7ead150d39e62be1137bb8bf086
Lucas Barbier <lucasbarbr@outlook.com>
2025-07-17 18:24:37 +0200

    correction des conditions ini pour les blasts

--- Init.h
+++ Init.h
@@ -492,30 +492,23 @@ namespace {
     real_t r = sqrt(xi*xi+yj*yj);
   
     if (r < 0.1) {
-      Q(j, i, IR) = 1.0;
-      Q(j, i, IU) = 0.0;
-      Q(j, i, IV) = 0.0;
-      Q(j, i, IW) = 0.0;
       Q(j, i, IP) = 10.0;
-      Q(j, i, IBX) = Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBY) = Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBZ) = 0.0;
     }
     else {
-      Q(j, i, IR) = 1.0;
-      Q(j, i, IU) = 0.0;
-      Q(j, i, IV) = 0.0;
-      Q(j, i, IW) = 0.0;
       Q(j, i, IP) = 0.1;
-      Q(j, i, IBX) = Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBY) = Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBZ) = 0.0;
     }
-    Q(j,i,IPSI)=0.0;
+    Q(j, i, IR) = 1.0;
+    Q(j, i, IU) = 0.0;
+    Q(j, i, IV) = 0.0;
+    Q(j, i, IW) = 0.0;
+    Q(j, i, IBX) = 0.5 * Kokkos::sqrt(2.0);
+    Q(j, i, IBY) = 0.5 * Kokkos::sqrt(2.0);
+    Q(j, i, IBZ) = 0.0;
+    Q(j, i, IPSI) = 0.0;
   }
 
 /**
-   * @brief MHD Blast Standard Configuration
+   * @brief MHD Blast Low Beta Configuration
    */
   KOKKOS_INLINE_FUNCTION
   void initBlastMHDLowBeta(Array Q, int i, int j, const DeviceParams &params) {
@@ -528,26 +521,20 @@ namespace {
     real_t r = sqrt(xi*xi+yj*yj);
   
     if (r < 0.1) {
-      Q(j, i, IR) = 1.0;
-      Q(j, i, IU) = 0.0;
-      Q(j, i, IV) = 0.0;
-      Q(j, i, IW) = 0.0;
       Q(j, i, IP) = 1000.0;
-      Q(j, i, IBX) = Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBY) = Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBZ) = 0.0;
     }
     else {
-      Q(j, i, IR) = 1.0;
-      Q(j, i, IU) = 0.0;
-      Q(j, i, IV) = 0.0;
-      Q(j, i, IW) = 0.0;
       Q(j, i, IP) = 0.1;
-      Q(j, i, IBX) = 250/Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBY) = 250/Kokkos::sqrt(2.0*M_PI);
-      Q(j, i, IBZ) = 0.0;
     }
-    Q(j,i,IPSI)=0.0;
+
+    Q(j, i, IR) = 1.0;
+    Q(j, i, IU) = 0.0;
+    Q(j, i, IV) = 0.0;
+    Q(j, i, IW) = 0.0;
+    Q(j, i, IBX) = 250.0/Kokkos::sqrt(2.0);
+    Q(j, i, IBY) = 250.0/Kokkos::sqrt(2.0);
+    Q(j, i, IBZ) = 0.0;
+    Q(j, i, IPSI) = 0.0;
   }
   /**
    * @brief MHD Rotated Shock Tube. Brio and Wu Shock Tube rotated by an angle \theta

8488ea74f2dfb26d2e02dbbd46c2fc6efbe94d4a
Lucas Barbier <lucasbarbr@outlook.com>
2025-07-21 15:14:33 +0200

    match tremblin 2020 setup for rotated shock tube

--- Init.h
+++ Init.h
@@ -536,33 +536,28 @@ namespace {
    */
   KOKKOS_INLINE_FUNCTION
   void initRotatedShockTube(Array Q, int i, int j, const DeviceParams &params) {
-    real_t x0 = 0.5 * (params.xmin+params.xmax);
-    real_t y0 = 0.5 * (params.ymin+params.ymax);
     Pos pos = getPos(params, i, j);
-    real_t xi = x0 - pos[IX];
-    real_t yj = y0 - pos[IY];
-    real_t r = sqrt(xi*xi+yj*yj);
-    real_t theta = Kokkos::atan(-2);
+    real_t theta = Kokkos::atan(-2.0);
 
-    real_t xt = tan(theta) * (pos[IX] - 0.5);
-    real_t yt = (pos[IY] - 0.5);
-    real_t B0 = 1.0 / sqrt(4 * M_PI);
+    real_t yt = tan(theta) * (pos[IY] - 0.5);
+    real_t xt = (pos[IX] - 0.5);
+    real_t B0 = 1.0 / sqrt(4.0 * M_PI);
     
     Q(j, i, IR) = 1.0;
     Q(j, i, IW) = 0.0;
     Q(j, i, IBX) = 5*B0 * (cos(theta) + sin(theta));
     Q(j, i, IBY) = 5*B0 * (cos(theta) - sin(theta));
     Q(j, i, IBZ) = 0.0;
-    Q(j,i,IPSI)=0.0;
+    Q(j, i, IPSI) = 0.0;
 
     if (xt < yt) {
-      Q(j, i, IU) = 10.0 * cos(theta);
+      Q(j, i, IU) =  10.0 * cos(theta);
       Q(j, i, IV) = -10.0 * sin(theta);
       Q(j, i, IP) = 20.0;
     }
     else {
       Q(j, i, IU) = -10.0 * cos(theta);
-      Q(j, i, IV) = 10.0 * sin(theta);
+      Q(j, i, IV) =  10.0 * sin(theta);
       Q(j, i, IP) = 1.0;
     }
   }
